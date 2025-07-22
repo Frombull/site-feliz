7948d2a9f630011a2d0094a51f281c27e536688b
Marco Di Toro <marcorrditoro@gmail.com>
2025-07-22 01:17:24 -0300

    Cool changes 😎

---
+++ src/lib/auth.ts
@@ -0,0 +1,67 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+import { NextAuthOptions } from "next-auth";
+
+const prisma = new PrismaClient();
+
+export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        CredentialsProvider({
+            name: 'Credentials',
+            credentials: {
+                email: { label: "Email", type: "text" },
+                password: {  label: "Password", type: "password" }
+            },
+            async authorize(credentials) {
+                if (!credentials?.email || !credentials.password) {
+                    return null;
+                }
+
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email: credentials.email
+                    }
+                });
+
+                if (!user || !user.password || !bcrypt.compareSync(credentials.password, user.password)) {
+                    return null;
+                }
+
+                return {
+                    id: user.id,
+                    email: user.email,
+                    name: user.name,
+                    image: user.image,
+                };
+            }
+        })
+    ],
+    session: {
+        strategy: "jwt" as const,
+    },
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                token.id = user.id;
+                token.name = user.name;
+                token.image = user.image;
+            }
+            return token;
+        },
+        async session({ session, token }) {
+            if (session.user) {
+                session.user.id = token.id as string;
+                session.user.name = token.name as string;
+                session.user.image = token.image as string;
+            }
+            return session;
+        },
+    },
+    pages: {
+        signIn: '/login',
+    },
+    secret: process.env.NEXTAUTH_SECRET,
+}; 
